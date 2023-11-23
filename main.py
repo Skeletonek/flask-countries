e1ddf738087ce59b52d56661f066d279c5ce4b01
Skeletonek <skeleton0199@gmail.com>
2023-11-23 12:00:25 +0100

    update

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap5
 import requests
+from database import tasks
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
-user = "Zdzisiek"
+user = "User"
 city = "Katowice"
-is_admin = True
+is_admin = False
 users = ["Zyzio", "Dyzio", "Krzysztof"]
 country = {
     "name":"Polska",
@@ -25,9 +28,11 @@ def hello_world():
                            country=country
                            )
 
+
 @app.route("/o-nas")
 def about_page():
     return "<h1>Informacje o nas...</h1>" 
+
 
 @app.route("/kraje")
 def countries_page():
@@ -37,6 +42,7 @@ def countries_page():
         return render_template("countries2.html", countries=countries)
     else:
         return f"Błąd {response.status_code}"
+
 
 @app.route("/kraje/<code>")
 def details_page(code):
@@ -63,12 +69,37 @@ def search_result_page():
     query = request.form["query"]
     response = requests.get(f"https://restcountries.com/v3.1/name/{query}")
     countries = response.json()
-    if response.ok:
-        countries = response.json()
-        return render_template("countries2.html", countries=countries)
-    else:
-        return f"Błąd {response.status_code}"
-    return "test"
+
+    return render_template("result.html",countries=countries)
+    # if response.ok:
+    #     countries = response.json()
+    #     return render_template("countries2.html", countries=countries)
+    # else:
+    #     return f"Błąd {response.status_code}"
+
+
+@app.route("/zadania")
+def all_tasks():
+    tasks_list = tasks.find({})
+    return render_template("tasks.html", tasks_list = tasks_list)
+
+
+@app.route("/dodaj-zadanie",methods=["GET","POST"])
+def create_task():
+    new_task = {
+        "title": request.form["tytul"],
+        "category": request.form["kategoria"]
+    }
+    tasks.insert_one(new_task)
+    return redirect("/zadania", code=302)
+
+
+@app.route("/usun-zadanie", methods=["GET", "POST"])
+def delete_task():
+    id = request.form["id"]
+    tasks.delete_one({"_id": ObjectId(id)})
+    return redirect("/zadania", code=302)
+
 
 @app.errorhandler(404)
 def not_found(e):
